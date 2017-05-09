f9088678c9d2836b9bab13e373c0361b269b2155
lvyating888 <625464771@qq.com>
2017-05-09 14:14:06 +0800

    qqw_index

---
+++ app_index/js/index_banner.swiper.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper('.swiper-container',{
+
+    pagination: '.pagination',
+    loop:true,
+    grabCursor: true,
+    paginationClickable: true,
+    autoplay: 2500,
+
+  })
+
